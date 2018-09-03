6a4b9cc5ba2b381432f8cbbb29b8ac9a7a3c5bbd
Jan von der Assen <jan.vda@gmx.ch>
2018-09-04 01:07:28 +0200

     Encapsulate behavior of an 'UnchallengedState' in GameStates.js

---
+++ game/GameStates.js
@@ -0,0 +1,37 @@
+function UnchallengedState (game) {
+  this.game = game;
+
+  this.handleCard = function (card, player) {
+    if(card.challenging) {
+      this.game.setNextPlayer(player);
+      this.game.challenger = player;
+      this.game.challengeTurnsRemaining = levelToTrys(card.level);
+      this.game.gameState = new ChallengedState(this.game);
+      return true;
+    } else {
+      this.game.setNextPlayer(player);
+      return true;
+    }
+  }
+
+}
+
+function ChallengedState (game) {
+  this.game = game;
+
+  this.handleCard = function (card, player) {
+    
+  }
+}
+
+function levelToTrys (level) {
+  if(level === 'ace') return 4;
+  if(level === 'king') return 3;
+  if(level === 'ober') return 2;
+  if(level === 'under') return 1;
+}
+
+module.exports = {
+  UnchallengedState,
+  ChallengedState
+}
